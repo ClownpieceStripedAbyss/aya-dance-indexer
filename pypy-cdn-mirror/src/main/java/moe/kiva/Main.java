bdea07048844d4bf9c8a0f8f97014666a1026eb9
imkiva <imkiva@islovely.icu>
2024-04-16 08:08:06 +0800

    misc: support .env file

--- pypy-cdn-mirror/src/main/java/moe/kiva/Main.java
+++ pypy-cdn-mirror/src/main/java/moe/kiva/Main.java
@@ -4,30 +4,73 @@ import com.google.gson.GsonBuilder;
 import kala.collection.immutable.ImmutableMap;
 import kala.collection.immutable.ImmutableSeq;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class Main {
+  record AppOpts(
+    @NotNull String outputDir,
+    @Nullable InetSocketAddress proxy,
+    int downloadDelay
+  ) {
+    public static @NotNull AppOpts parseEnv() throws IOException {
+      var envContents = Files.readString(Path.of(".env"), StandardCharsets.UTF_8);
+      var p = new Properties();
+      p.load(new StringReader(envContents));
+
+      var mirrorDownloadProxy = p.getProperty("MIRROR_DOWNLOAD_PROXY", "");
+      var videoPath = p.getProperty("VIDEO_PATH", "./pypydance-song");
+      var mirrorDownloadDelay = p.getProperty("MIRROR_DOWNLOAD_DELAY", "30");
+
+      if (mirrorDownloadProxy.isBlank()) {
+        return new AppOpts(
+          videoPath,
+          null,
+          Integer.parseInt(mirrorDownloadDelay)
+        );
+      }
+
+      var split = mirrorDownloadProxy.split(":");
+      if (split.length != 2) throw new IllegalArgumentException("Invalid proxy format");
+
+      var proxyHost = split[0];
+      var proxyPort = Integer.parseInt(split[1]);
+
+      return new AppOpts(
+        videoPath,
+        new InetSocketAddress(proxyHost, proxyPort),
+        Integer.parseInt(mirrorDownloadDelay)
+      );
+    }
+  }
+
   public static void main(String[] args) throws IOException {
+    var opts = AppOpts.parseEnv();
+    System.out.printf("Video path: %s%n", opts.outputDir);
+    System.out.printf("Proxy: %s%n", opts.proxy);
+    System.out.printf("Download delay: %d%n", opts.downloadDelay);
+
     var songList = SongPyPyApiParser.parse();
 
-    downloadVideos(songList);
+    downloadVideos(opts, songList);
     generateVidViz(songList);
   }
 
-  private static void downloadVideos(ImmutableSeq<Song> songList) {
+  private static void downloadVideos(@NotNull AppOpts opts, @NotNull ImmutableSeq<Song> songList) {
     var downloader = Downloader.create(
       songList,
-      "./pypydance-song",
-      30,
-      // Better use an IP pool, as we are crawling a lot of videos
-      new InetSocketAddress("127.0.0.1", 10809)
+      opts.outputDir,
+      opts.downloadDelay,
+      opts.proxy
     );
     downloader.downloadAllMulti();
   }
