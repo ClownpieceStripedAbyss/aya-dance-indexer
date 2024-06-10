a87a977c7257227707785e3cd92ee8163e48ffad
imkiva <imkiva@islovely.icu>
2024-06-10 18:28:29 +0800

    mirror: remove `kr3`

--- mirror/src/main/java/moe/kiva/Downloader.java
+++ mirror/src/main/java/moe/kiva/Downloader.java
@@ -45,7 +45,6 @@ public record Downloader(
   private static final @NotNull ImmutableSeq<String> CDN_LIST = ImmutableSeq.of(
     "http://storage-kr1.llss.io",
     "http://storage-kr2.llss.io",
-    "http://storage-kr3.llss.io",
     "http://storage-cf.llss.io"
   );
 
