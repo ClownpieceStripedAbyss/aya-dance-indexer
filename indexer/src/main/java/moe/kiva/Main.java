2ff46377ee011db2ab45388778f846187a88c3c9
imkiva <imkiva@islovely.icu>
2024-06-20 23:16:40 +0800

    indexer: `flip` should be explicitly specified

--- indexer/src/main/java/moe/kiva/Main.java
+++ indexer/src/main/java/moe/kiva/Main.java
@@ -8,8 +8,8 @@ import org.jetbrains.annotations.NotNull;
 
 public class Main {
   public static void main(String @NotNull [] args) {
-    if (args.length != 9 && args.length != 4) {
-      System.err.println("Usage: <url> <id> <catId> <catName> [<volume> <flip> <titleOverride> <startOverride> <endOverride>]");
+    if (args.length != 9 && args.length != 5) {
+      System.err.println("Usage: <url> <id> <catId> <catName> <flip> [<volume> <titleOverride> <startOverride> <endOverride>]");
       System.exit(1);
     }
 
@@ -32,11 +32,11 @@ public class Main {
     var id = Integer.parseInt(args[1]);
     var catId = Integer.parseInt(args[2]);
     var catName = args[3];
+    var flip = Boolean.parseBoolean(args[4]);
 
-    boolean shorterVersion = args.length == 4;
+    boolean shorterVersion = args.length == 5;
 
-    var volume = shorterVersion ? 0.35f : Float.parseFloat(args[4]);
-    var flip = !shorterVersion && Boolean.parseBoolean(args[5]);
+    var volume = shorterVersion ? 0.35f : Float.parseFloat(args[5]);
     var titleOverride = shorterVersion ? Option.<String>none() : args[6].equals("null") ? Option.<String>none() : Option.some(args[6]);
     var startOverride = shorterVersion ? Option.<Integer>none() : args[7].equals("null") ? Option.<Integer>none() : Option.some(Integer.parseInt(args[7]));
     var endOverride = shorterVersion ? Option.<Integer>none() : args[8].equals("null") ? Option.<Integer>none() : Option.some(Integer.parseInt(args[8]));
